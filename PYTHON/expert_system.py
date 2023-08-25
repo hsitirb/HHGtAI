c467c9a1e9a2bec1e2df5fa7ae93f43847cf839a
mnjones <mnjones@gmail.com>
2023-08-25 18:21:33 +0100

    <MINOR> Linting fixes

--- PYTHON/expert_system.py
+++ PYTHON/expert_system.py
@@ -12,7 +12,8 @@ print("------")
 HYPOTHESES : int = 100  # The number of different hypotheses
 EVIDENCE : int = 4      # The number of different items of evidence
 
-POSSIBLE : list[bool] = [True] * HYPOTHESES  # Set to True initially, and False when the hypothesis is ruled out
+# Set to True initially, and False when the hypothesis is ruled out
+POSSIBLE : list[bool] = [True] * HYPOTHESES  
 A : list[list] = []                          # Answer submitted by user
 Q : list[list] = []                          # Name of question
 R : list[list] = []                          # Correct reply
@@ -23,15 +24,15 @@ N : list = []                                # Name of hypothesis
 COUNTER = 0
 with open("data.dat") as datafile:
     # INPUT loop
-    while data := datafile.readline() != "999":
+    while (data := datafile.readline()) != "999":
         COUNTER += 1
         N.append(data)
-        for I in range(EVIDENCE):
+        for _ in range(EVIDENCE):
             data = datafile.readline()
             for J in range(len(data)):
                 if data[J] == " ":
                     L = J
                     J = len(data)
-                    Q[COUNTER][I] = data[:L]
-                    R[COUNTER][I] = data[-L:]
+                    Q[COUNTER].append(data[:L])
+                    R[COUNTER].append(data[L + 1:])
     
