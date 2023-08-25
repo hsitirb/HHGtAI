6db66544fd0c8a0627cf25c4950861d9b675e820
hsitirb <114746916+hsitirb@users.noreply.github.com>
2023-08-25 17:54:45 +0000

    Lines 160-240: Input loop

--- PYTHON/expert_system.py
+++ PYTHON/expert_system.py
@@ -17,3 +17,21 @@ A : list[list] = []                          # Answer submitted by user
 Q : list[list] = []                          # Name of question
 R : list[list] = []                          # Correct reply
 N : list = []                                # Name of hypothesis
+
+
+##### Read knowledge base into above lists #####
+COUNTER = 0
+with open("data.dat") as datafile:
+    # INPUT loop
+    while data := datafile.readline() != "999":
+        COUNTER += 1
+        N.append(data)
+        for I in range(EVIDENCE):
+            data = datafile.readline()
+            for J in range(len(data)):
+                if data[J] == " ":
+                    L = J
+                    J = len(data)
+                    Q[COUNTER][I] = data[:L]
+                    R[COUNTER][I] = data[-L:]
+    
