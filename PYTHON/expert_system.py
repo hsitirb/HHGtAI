26f8b120452feeeabb2aaeae82cbd51883f25414
mnjones <mnjones@gmail.com>
2023-08-27 15:57:56 +0100

    Lines 10-60 - Python module docstring

---
+++ PYTHON/expert_system.py
@@ -0,0 +1,11 @@
+# 10 REM ****************************
+# 20 REM ** Listeing 2.1           **
+# 30 REM ** Simple, Deterministic, **
+# 40 REM ** Forward-Chaining       **
+# 50 REM ** EXPERT SYSTEM          **
+# 60 REM ****************************
+"""
+Listeing 2.1
+Simple, Deterministic, Forward-Chaining
+EXPERT SYSTEM
+"""
