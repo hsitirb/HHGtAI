fef21333c9635c25f7d2c776a4e1e3bdf959499a
mnjones <mnjones@gmail.com>
2023-08-28 08:03:32 +0100

    Lines 260-350: Start questioning the user

--- PYTHON/expert_system.py
+++ PYTHON/expert_system.py
@@ -72,3 +72,61 @@ with open("data.dat") as data_file:
                     Q_str[COUNTER, I_idx] = data[:L_idx]
                     R_str[COUNTER, I_idx] = data[L_idx + 1 :]
 HYPOTHESES = COUNTER
+
+# 260 REM -- Start questioning user:
+# 270 FOR COUNTER%=1 TO HYPOTHESES%
+# 280 IF POSSIBLE%(COUNTER%)=0 THEN GOTO 540
+# 290 FOR I%=1 TO EVIDENCE%: IF A$(COUNTER%,I%) <> "" THEN GOTO 500
+# 300 PRINT: PRINT "What is ";Q$(COUNTER%,I%);" ?"
+# 310 PRINT "(Possible replies are :"
+# 320 FOR Q%=1 TO HYPOTHESES%: IF POSSIBLE%(Q%)=1 THEN PRINT SPC(20);R$(Q%,I%)
+# 330 NEXT Q%
+# 340 PRINT "or '*' for don't-care or '< x' or '> x' for ranges.)"
+# 350 INPUT A$(COUNTER%,I%)
+
+##### Start questioning user #####
+for COUNTER in range(1, HYPOTHESES + 1):
+    if POSSIBLE[COUNTER] == 0:
+        continue
+    for I_idx in range(1, EVIDENCE + 1):
+        if A_str[COUNTER, I_idx] != "":
+            continue
+        print()
+        print("What is", Q_str[COUNTER, I_idx], "?")
+        print("(Possible replies are :")
+        for Q_idx in range(1, HYPOTHESES + 1):
+            if POSSIBLE[Q_idx] == 1:
+                print(" " * 20, R_str[Q_idx, I_idx])
+        print("or '*' for don't-care or '< x' or '> x' for ranges.)", end="")
+        A_str[COUNTER, I_idx] = input(": ")
+
+# 360 REM -- Look for other questions this might also apply to:
+# 370 FOR Q%=COUNTER%+1 TO HYPOTHESES%
+# 380 FOR E%=1 to EVIDENCE%
+# 390 IF Q$(Q%,E%)=Q$(COUNTER,I%) THEN A$(Q%,E%)=A$(COUNTER%,I%)
+# 400 NEXT E%: NEXT Q%
+
+
+# 410 REM -- Look for hypotheses that can be discounted due to this answer:
+# 420 FOR H%=1 TO HYPOTHESES%
+# 430 FOR E%=1 TO EVIDENCE%
+# 440 POSS%=POSSIBLE%(H%): GOSUB 1500: REM test reply against question
+# 450 IF POSS%<POSSIBLE%(H%) THEN PRINT: PRINT N$(H%);" is ruled out." PRINT "Would you like to know why (Y/N)";: INPUT A$: IF A$="y" OR A$="Y" THEN PRINT
+# 460 POSSIBLE%(H%)=POSS% : REM result of test routine
+# 470 NEXT E%: NEXT H%
+# 480 P%=0: FOR Q%=1 TO HYPOTHESES%: P%=P%+POSSIBLE%(Q%): NEXT Q%
+# 490 IF P%=0 THEN PRINT "No possibilites left!": I%=EVIDENCE%: COUNTER%=HYPOTHESES%
+# 500 NExT I%: REM on to next question
+
+
+# 510 REM -- Display any theories that match user's replies:
+# 520 IF POSSIBLE%(COUNTER%) THEN PRINT CHR$(7): PRINT N$(COUNTER%);" is possible.": PRINT "Would you like to know (Y/N)": INPUT A$: IF A$="y" OR A$="Y" THEN GOSUB 1200
+
+
+# 530 REM -- Now try another theory
+# 540 NEXT COUNTER%
+# 550 PRINT: PRINT "Wold you like another run (Y/N) ";: INPUT A$
+# 560 IF A$="y" OR A$="Y" THEN GOSUB 1200
+# 570 IF A$="n" OR A$="N" THEN END
+# 580 GOTO 260: REM Loop back
+#
